e822a246557ead66723ca12e736db1fd4befa581
Michael Vitz <michael.vitz@innoq.com>
2015-12-09 23:57:06 +0100

    Initial commit.

---
+++ src/main/java/com/innoq/urls/api/UrlsApiApplication.java
@@ -0,0 +1,13 @@
+package com.innoq.urls.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlsApiApplication.class, args);
+    }
+
+}
