51204a811b823b7c76abe30f15acf8167f4f3262
Michael Vitz <michael.vitz@innoq.com>
2015-12-10 01:44:37 +0100

    Add basic logging

--- src/main/java/com/innoq/urls/api/domain/HashService.java
+++ src/main/java/com/innoq/urls/api/domain/HashService.java
@@ -4,11 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class HashService {
+
+    private static final Logger LOGGER = LogManager.getLogger(HashService.class);
 
     private final Map<String, String> hashToValue = new HashMap<>();
     private final Hasher hasher;
@@ -20,12 +24,14 @@ public final class HashService {
 
     public String hash(String value) {
         final String hash = hasher.hash(value);
+        LOGGER.info("'{}' hashed as '{}'", value, hash);
         hashToValue.put(hash, value);
         return hash;
     }
 
     public Optional<String> resolve(String hash) {
         final String value = hashToValue.get(hash);
+        LOGGER.info("'{}' resolved to '{}'", hash, value);
         return Optional.ofNullable(value);
     }
 
